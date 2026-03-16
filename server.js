26df95efce6beab4d151bab72deac9cec428470d
Muhammad Jawad Raza <141541759+Yourker121@users.noreply.github.com>
2026-03-16 16:32:32 +0500

    fix cors vercel

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin:
+      "https://fyp-frontend-ohg4-p0uxxgej6-yourker121s-projects.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
